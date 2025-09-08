80e4fe13deecdc661e2201d8caa936375bd5eb50
Helmi Fauzi <helmi.fauzi1@ihc.id>
2025-09-08 10:58:33 +0700

    fix/tevert-usedisableinspect

--- apps/web/src/components/MainContent/index.tsx
+++ apps/web/src/components/MainContent/index.tsx
@@ -4,9 +4,6 @@ import { useDisableInspect } from "@/lib/hooks/useDisableInspect"
 import styles from "./layout.module.scss"
 
 export const MainContent = ({children} : any) => {
-  if (process.env.NODE_ENV === "production") {
-    useDisableInspect();
-  }
   
   return (
     <div className={styles.page}>
